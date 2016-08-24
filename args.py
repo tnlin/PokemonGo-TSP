3a47dba6c76a2c7def7f169b05dddfeb6c1e8cb4
JeremyChen <jeremychen0127@gmail.com>
2016-08-23 23:44:11 -0400

    Added args parser

---
+++ args.py
@@ -0,0 +1,33 @@
+import argparse
+
+def parse_args():
+    arg_parser = argparse.ArgumentParser()
+
+    # Help messages for each option
+    helps = {}
+    helps['data'] = 'Data to calculate TSP'
+    helps['markov'] = 'Coefficient to multiply the number of locations'
+    helps['halt'] = 'Optimization threshold: stability of solution as program halts'
+
+    arg_parser.add_argument(
+        '-d', '--data', 
+        choices=['nctu', 'nthu', 'thu']
+        help=helps['data']
+    ) 
+
+    arg_parser.add_argument(
+        '-m', '--markov-coefficient', 
+        type=int,
+        default=10,
+        help=helps['markov']
+    )
+
+    arg_parser.add_argument(
+        '--halt', 
+        help=helps['halt'],
+        type=int,
+        default=150,
+        metavar='THRESHOLD'
+    )
+
+    return arg_parser.parse_args()
